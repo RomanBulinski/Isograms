8d906e0c5e6c6b19bdab1f8cbb1dd860715ee87e
rbulin <roman.bulinski@pentacomp.pl>
2020-01-03 20:50:21 +0100

    Implemented code

--- src/main/java/Isograms.java
+++ src/main/java/Isograms.java
@@ -1,8 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Isograms {
 
-    public static boolean  isIsogram(String str) {
-
-        return true;
+    public static boolean isIsogram(String stri) {
+        String str = stri.toLowerCase();
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        for (int i = 0; i < str.length(); i++) {
+            map.put(str.charAt(i), 1);
+        }
+        return map.size() == str.length();
     }
-
 }
